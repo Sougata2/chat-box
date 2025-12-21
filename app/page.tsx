4f7eef31e3b34b3f35e33bb64edfce52c8326eb4
sougata <sougata19582001@gmail.com>
2025-12-21 17:53:18 +0530

    if token exists then redirect to chat page else redirect to sign-in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
-export default function Home() {
-  redirect("/sign-in");
+export default async function Home() {
+  const token = (await cookies()).get("Authorization")?.value;
+  if (!token) {
+    return redirect("/sign-in");
+  }
+  return redirect("/chat");
 }
