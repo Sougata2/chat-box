d65b905e9e0d7b726873e03b95cb1161c7077f4a
sougata <sougata19582001@gmail.com>
2025-12-21 17:52:03 +0530

    setting up redux providers and adding it layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next";
 import { Toaster } from "@/components/ui/sonner";
+
+import Providers from "./providers";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <Toaster richColors position="top-center" duration={3000} />
+        <Providers>
+          {children}
+          <Toaster richColors position="top-center" duration={3000} />
+        </Providers>
       </body>
     </html>
   );
