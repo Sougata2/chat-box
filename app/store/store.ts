428c2d5d64ad31c876dde18b5ab869d26ba70561
sougata <sougata19582001@gmail.com>
2025-12-21 23:33:05 +0530

    registered room slice in redux store

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "@/app/store/userSlice";
+import roomReducer from "@/app/store/roomSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    rooms: roomReducer,
   },
 });
 
