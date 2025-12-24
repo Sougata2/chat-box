e8a5f07e55e7b2e49bfa0637ff4727cc381dad3d
sougata <sougata19582001@gmail.com>
2025-12-25 00:45:59 +0530

    added semi colon after the particapant's name

--- components/RoomBlock.tsx
+++ components/RoomBlock.tsx
@@ -28,7 +28,7 @@ function RoomBlock({
         <div className="text-xs">
           {room.messages[0].sender?.email === loggedInUser?.email
             ? "You: "
-            : room.messages[0].sender?.firstName}
+            : `${room.messages[0].sender?.firstName}: `}
           {room.messages[0].message}
         </div>
       </div>
