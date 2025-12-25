88d682d9e3d016391000f537b5a406388249ce97
sougata <sougata19582001@gmail.com>
2025-12-25 16:55:27 +0530

    updated the room block to show data according to room summary api.

--- components/RoomBlock.tsx
+++ components/RoomBlock.tsx
@@ -9,27 +9,24 @@ function RoomBlock({
   room: Room;
   loggedInUser: User | null;
 }) {
-  const otherParticipant = room.participants.find(
-    (u: User) => u.email !== loggedInUser?.email
-  );
   return (
     <div className="flex gap-2.5 items-center rounded-xl px-2 py-4 hover:bg-slate-100 cursor-pointer">
       <Avatar>
         <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
         <AvatarFallback>
-          {otherParticipant?.firstName.substring(0, 1)}
-          {otherParticipant?.lastName.substring(0, 1)}
+          {room?.otherParticipantFirstName?.substring(0, 1)}
+          {room?.otherParticipantLastName?.substring(0, 1)}
         </AvatarFallback>
       </Avatar>
       <div className="flex flex-col gap-0.5">
         <div className="text-slate-700 font-medium">
-          {otherParticipant?.firstName} {otherParticipant?.lastName}
+          {room?.otherParticipantFirstName} {room?.otherParticipantLastName}
         </div>
         <div className="text-xs">
-          {room.messages[0].sender?.email === loggedInUser?.email
+          {room?.latestMessageSenderEmail === loggedInUser?.email
             ? "You: "
-            : `${room.messages[0].sender?.firstName}: `}
-          {room.messages[0].message}
+            : `${room?.otherParticipantFirstName}: `}
+          {room?.latestMessage}
         </div>
       </div>
     </div>
