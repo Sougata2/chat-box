afdc86d9fd9a01c02f9164d95055b0e98b866248
sougata <sougata19582001@gmail.com>
2025-12-25 16:52:25 +0530

    throw error if token is expired

--- app/clients/predicates.ts
+++ app/clients/predicates.ts
@@ -3,6 +3,9 @@ import Cookies from "js-cookie";
 
 export const requestPredicate = (config: InternalAxiosRequestConfig) => {
   const token = Cookies.get("Authorization");
+  if (!token) {
+    throw new Error("Login Required");
+  }
   config.headers.set("Authorization", `Bearer ${token}`);
   return config;
 };
