6ec6cb47d182376498b6bae70bbb81444619ecf8
sougata <sougata19582001@gmail.com>
2025-12-28 00:10:01 +0530

    added method to unshift the latest message any participant sends message

--- app/store/chatSlice.ts
+++ app/store/chatSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Room } from "../types/room";
+import { Message, Room } from "../types/room";
 
 export interface chatState {
   room: Room | null;
@@ -16,8 +16,13 @@ const chatSlice = createSlice({
     selectRoom(state, action: PayloadAction<Room>) {
       state.room = action.payload;
     },
+    unShiftMessage(state, action: PayloadAction<Message>) {
+      if (!state.room) return;
+      if (!state.room.messages) state.room.messages = [];
+      state.room.messages.unshift(action.payload);
+    },
   },
 });
 
-export const { selectRoom } = chatSlice.actions;
+export const { selectRoom, unShiftMessage } = chatSlice.actions;
 export default chatSlice.reducer;
