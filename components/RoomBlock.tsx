4461f58db51f75c5623b9360d22c280b1a65d5a3
sougata <sougata19582001@gmail.com>
2025-12-28 21:05:47 +0530

    changed the message font styling

--- components/RoomBlock.tsx
+++ components/RoomBlock.tsx
@@ -26,7 +26,7 @@ function RoomBlock({
         <div className="text-slate-700 font-medium capitalize">
           {otherParticipant?.firstName} {otherParticipant?.lastName}
         </div>
-        <div className="text-xs line-clamp-1">
+        <div className="text-[13px] font-medium text-slate-500 line-clamp-1">
           {(room.messages[0].senderEmail || room.messages[0].sender.email) ===
           loggedInUser?.email
             ? "You: "
