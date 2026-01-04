fe746da2271a0548a12c6e21d6e93f95b148fdd8
sougata <sougata19582001@gmail.com>
2026-01-05 00:06:19 +0530

    fetching contacts and displaying the list.

--- components/NewChatMenu.tsx
+++ components/NewChatMenu.tsx
@@ -1,8 +1,30 @@
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { useCallback, useEffect, useState } from "react";
 import { FaArrowLeft } from "react-icons/fa6";
 import { MdGroupAdd } from "react-icons/md";
+import { toastError } from "./toastError";
 import { Input } from "./ui/input";
+import { User } from "@/app/types/user";
+import { chat } from "@/app/clients/chatClient";
 
 function NewChatMenu({ closeNewChatMenu }: { closeNewChatMenu: () => void }) {
+  const [contacts, setContacts] = useState<User[]>();
+
+  const fetchContacts = useCallback(async () => {
+    try {
+      const response = await chat.get("/users/contacts");
+      setContacts(response.data);
+    } catch (error) {
+      toastError(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      await fetchContacts();
+    })();
+  }, [fetchContacts]);
+
   return (
     <div className="">
       <div className="py-4 px-4 flex gap-5 items-center">
@@ -34,8 +56,30 @@ function NewChatMenu({ closeNewChatMenu }: { closeNewChatMenu: () => void }) {
             </span>
           </button>
         </div>
-        <div className="px-4">
+        <div className="px-4 flex flex-col gap-3.5">
           <div className="font-semibold text-lg text-slate-600">Contacts</div>
+          {contacts?.map((c) => (
+            <div
+              key={c.id}
+              className="flex flex-col gap-2 px-2 py-4 hover:rounded-xl hover:bg-slate-100 cursor-pointer"
+            >
+              <div className="flex items-center gap-3">
+                <Avatar>
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                  />
+                  <AvatarFallback className="capitalize">
+                    <span>{c.firstName}</span>
+                    <span>{c.lastName}</span>
+                  </AvatarFallback>
+                </Avatar>
+                <div>
+                  {c.firstName} {c.lastName}
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
