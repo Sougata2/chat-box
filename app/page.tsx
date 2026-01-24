ab39376b3141aef806650eee5ce6be3d84417944
sougata <sougata19582001@gmail.com>
2026-01-25 02:29:44 +0530

    replaced the redirect with useRouter

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
+"use client";
 
-export default async function Home() {
-  const token = (await cookies()).get("Authorization")?.value;
-  if (!token) {
-    return redirect("/sign-in");
-  }
-  return redirect("/chat");
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "./store/store";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+  const accessToken = useSelector((state: RootState) => state.user.accessToken);
+  const user = useSelector((state: RootState) => state.user.user);
+
+  useEffect(() => {
+    if (!accessToken || !user) {
+      router.replace("/sign-in");
+    } else {
+      router.replace("/chat");
+    }
+  }, [accessToken, router, user]);
+
+  return null;
 }
