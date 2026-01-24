033990887ff1ec351cb5b7339a7ec7959ae6dc75
sougata <sougata19582001@gmail.com>
2026-01-25 02:28:53 +0530

    added parent component to call refresh token api and make sure the store is populated.

---
+++ app/AuthProvider.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import { resetUser, setAuth } from "./store/userSlice";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "./store/store";
+import { useRouter } from "next/navigation";
+import { auth } from "./clients/authClient";
+import { User } from "./types/user";
+
+import React from "react";
+
+function AuthProvider({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const dispatch = useDispatch<AppDispatch>();
+  const [ready, setReady] = useState<boolean>(false);
+
+  const restoreSession = useCallback(async () => {
+    try {
+      const response = await auth.post("/auth/refresh");
+
+      dispatch(
+        setAuth({
+          user: {
+            email: response.data.email,
+            firstName: response.data.firstName,
+            lastName: response.data.lastName,
+          } as User,
+          accessToken: response.data.token,
+          expireAt: response.data.expiration,
+        }),
+      );
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      dispatch(resetUser());
+      router.push("/sign-in");
+    } finally {
+      setReady(true);
+    }
+  }, [dispatch, router]);
+
+  useEffect(() => {
+    (async () => {
+      await restoreSession();
+    })();
+  }, [restoreSession]);
+
+  if (!ready) return <div>...loading</div>;
+
+  return <>{children}</>;
+}
+
+export default AuthProvider;
