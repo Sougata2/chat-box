911169e7b6974191564ee2c2dc7aa3c693dc435b
sougata <sougata19582001@gmail.com>
2026-01-25 02:31:42 +0530

    added response predicates to auto fetch access token if the access token expires.

--- app/clients/chatClient.ts
+++ app/clients/chatClient.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
-import { requestPredicate } from "./predicates";
+import { requestPredicate, responsePredicate } from "./predicates";
 
 export const chat = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}/chat-service`,
 });
 
-chat.interceptors.request.use((config) => requestPredicate(config));
+chat.interceptors.request.use(
+  (config) => requestPredicate(config),
+  Promise.reject,
+);
+
+chat.interceptors.response.use((response) => response, responsePredicate);
--- app/clients/predicates.ts
+++ app/clients/predicates.ts
@@ -1,5 +1,27 @@
-import { InternalAxiosRequestConfig } from "axios";
+import { AxiosError, InternalAxiosRequestConfig } from "axios";
+import { resetUser, setAuth } from "../store/userSlice";
 import { store } from "../store/store";
+import { auth } from "./authClient";
+import { User } from "../types/user";
+
+import axios from "axios";
+
+let isRefreshing: boolean = false;
+let failedQueue: {
+  resolve: (token: string) => void;
+  reject: (error: AxiosError) => void;
+}[] = [];
+
+const processQueue = (error: AxiosError | null, token: string | null) => {
+  failedQueue.forEach((p) => {
+    if (error) {
+      p.reject(error);
+    } else {
+      p.resolve(token!);
+    }
+  });
+  failedQueue = [];
+};
 
 export const requestPredicate = (config: InternalAxiosRequestConfig) => {
   const token = store.getState().user.accessToken;
@@ -8,4 +30,54 @@ export const requestPredicate = (config: InternalAxiosRequestConfig) => {
   }
   config.withCredentials = true;
   return config;
+};
+
+export const responsePredicate = async (error: AxiosError) => {
+  const originalRequest = error.config as InternalAxiosRequestConfig & {
+    _retry?: boolean;
+  };
+
+  if (error.response?.status === 401 && !originalRequest._retry) {
+    if (isRefreshing) {
+      return new Promise((resolve, reject) => {
+        failedQueue.push({
+          resolve: (token: string) => {
+            originalRequest.headers.Authorization = `Bearer ${token}`;
+            resolve(axios(originalRequest));
+          },
+          reject,
+        });
+      });
+    }
+
+    originalRequest._retry = true;
+    isRefreshing = true;
+
+    try {
+      const response = await auth.post("/auth/refresh");
+
+      const { token, expiration, email, firstName, lastName } = response.data;
+
+      store.dispatch(
+        setAuth({
+          accessToken: token,
+          expireAt: expiration,
+          user: { email, firstName, lastName } as User,
+        }),
+      );
+
+      processQueue(null, token);
+
+      originalRequest.headers.Authorization = `Bearer ${token}`;
+      return axios(originalRequest);
+    } catch (err) {
+      processQueue(err as AxiosError, null);
+      store.dispatch(resetUser());
+      return Promise.reject(err);
+    } finally {
+      isRefreshing = false;
+    }
+  }
+
+  return Promise.reject(error);
 };
