c16518d1983e28bc46b360397f29240982e98ddb
sougata <sougatadas@cdac.in>
2026-01-28 17:56:05 +0530

    added page reducer to page store

--- app/store/store.ts
+++ app/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "@/app/store/userSlice";
 import roomReducer from "@/app/store/roomSlice";
 import chatReducer from "@/app/store/chatSlice";
+import pageReducer from "@/app/store/pageSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     rooms: roomReducer,
     chat: chatReducer,
+    page: pageReducer,
   },
 });
 
