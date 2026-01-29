a84feeba5895de9a429d9258dd107a75c438476e
sougata <sougatadas@cdac.in>
2026-01-29 15:48:09 +0530

    added profile stack. Added generice stack page, pop page and reset stack reducers

--- app/store/pageSlice.ts
+++ app/store/pageSlice.ts
@@ -1,19 +1,28 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Page } from "../types/page";
+import { Page, PageLocator } from "../types/page";
 
 export interface PageState {
   rooms: Page[];
   window: Page[];
+  profile: Page[];
 }
 
 const initialState: PageState = {
   rooms: [],
   window: [],
+  profile: [],
 };
 
 const rooms = {
   name: "room",
   import: "@/components/Rooms",
+  closeable: false,
+} as Page;
+
+const profile = {
+  name: "profile",
+  import: "@/components/Profile",
+  closeable: false,
 } as Page;
 
 const pageSlice = createSlice({
@@ -22,35 +31,39 @@ const pageSlice = createSlice({
   reducers: {
     initializePages(state) {
       state.rooms.unshift(rooms);
+      state.profile.unshift(profile);
     },
-    pushRooms(state, action: PayloadAction<Page>) {
-      state.rooms.push(action.payload);
+    stackPage(state, action: PayloadAction<PageLocator>) {
+      const stack = action.payload.stack;
+      const page = action.payload.page;
+
+      if (!page) {
+        throw new Error("Page is not available");
+      }
+
+      if (state[stack].length > 0 && state[stack][0].import === page.import) {
+        return;
+      } else {
+        state[stack].unshift(page);
+      }
     },
-    popRooms(state) {
-      state.rooms.shift();
+    popPage(state, action: PayloadAction<PageLocator>) {
+      const stack = action.payload.stack;
+      state[stack].pop();
     },
-    pushWindow(state, action: PayloadAction<Page>) {
-      state.window.unshift(action.payload);
-    },
-    popWindow(state) {
-      state.window.shift();
-    },
-    resetWindow(state) {
-      state.window = [];
-    },
-    resetRooms(state) {
-      state.rooms = [rooms];
+    resetStack(state, action: PayloadAction<PageLocator>) {
+      const stack = action.payload.stack;
+      const defaultPage = action.payload.defaultPage;
+
+      if (defaultPage) {
+        state[stack] = [defaultPage];
+      } else {
+        state[stack] = [];
+      }
     },
   },
 });
 
-export const {
-  initializePages,
-  popRooms,
-  popWindow,
-  pushRooms,
-  pushWindow,
-  resetRooms,
-  resetWindow,
-} = pageSlice.actions;
+export const { popPage, stackPage, resetStack, initializePages } =
+  pageSlice.actions;
 export default pageSlice.reducer;
