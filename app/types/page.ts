7d3388ea9501a08e212963510b8250e0a0c34ce4
sougata <sougatadas@cdac.in>
2026-01-29 15:31:44 +0530

    added PageLocator interface.

--- app/types/page.ts
+++ app/types/page.ts
@@ -1,4 +1,13 @@
+import { StackKey } from "@/components/PageRenderer";
+
 export interface Page {
   name: string;
   import: string;
+  closeable: boolean;
+}
+
+export interface PageLocator {
+  stack: StackKey;
+  page: Page | null;
+  defaultPage: Page | null;
 }
