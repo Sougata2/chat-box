270223d87d3d8ec7c1418d71ba108949d3b49af1
sougata <sougata19582001@gmail.com>
2026-01-29 23:02:56 +0530

    instead of ristricting the room block width, ristrcted the width of the message span

--- components/RoomBlock.tsx
+++ components/RoomBlock.tsx
@@ -17,7 +17,7 @@ function RoomBlock({
   );
 
   return (
-    <div className="flex gap-2.5 items-center rounded-xl px-2 py-4 hover:bg-slate-100 cursor-pointer overflow-hidden max-w-xs">
+    <div className="flex gap-2.5 items-center rounded-xl px-2 py-4 hover:bg-slate-100 cursor-pointer overflow-hidden">
       {room?.groupName && <GroupAvatar />}
       {!room?.groupName && (
         <Avatar className="h-10 w-10">
@@ -49,7 +49,7 @@ function RoomBlock({
               <TbChecks size={16} className="shrink-0 text-slate-500" />
             )}
 
-            <span className="flex-1 min-w-0 truncate">
+            <span className="flex-1 min-w-0 truncate max-w-3xs">
               {(room.messages[room.uuids[0]]?.senderEmail ||
                 room.messages[room.uuids[0]]?.sender?.email) ===
               loggedInUser?.email
