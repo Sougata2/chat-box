5d5a2bd97cbb236cabc4d3cdfe25b908bc258a05
sougata <sougata19582001@gmail.com>
2026-01-30 01:47:02 +0530

    added Registry object. Added  modified component display when component is of type registry object.

--- components/PageRenderer.tsx
+++ components/PageRenderer.tsx
@@ -1,19 +1,31 @@
-import { useSelector } from "react-redux";
-import { RootState } from "@/app/store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/app/store/store";
+import { PageLocator } from "@/app/types/page";
+import { popPage } from "@/app/store/pageSlice";
+import { FaXmark } from "react-icons/fa6";
 
+import NewChatMenu from "./NewChatMenu";
 import Profile from "./Profile";
 import Window from "@/components/Window";
 import Rooms from "@/components/Rooms";
+import React from "react";
 
-const pageRegistry: Record<string, React.FC> = {
+interface RegistryObject {
+  label: string | null;
+  component: React.FC;
+}
+
+export const pageRegistry: Record<string, RegistryObject | React.FC> = {
   room: Rooms,
   window: Window,
   profile: Profile,
+  newChatMenu: { label: "New Chat", component: NewChatMenu },
 };
 
 export type StackKey = "rooms" | "window" | "profile";
 
 function PageRenderer({ stack }: { stack: StackKey }) {
+  const dispatch = useDispatch<AppDispatch>();
   const pages = useSelector((state: RootState) => state.page[stack]);
 
   if (pages.length <= 0) return null;
@@ -22,9 +34,28 @@ function PageRenderer({ stack }: { stack: StackKey }) {
 
   const Component = pageRegistry[page.name];
 
+  if (typeof Component === "function") {
+    return <Component />;
+  }
+
   if (!Component) return null;
 
-  return <Component />;
+  return (
+    <div className="h-full min-h-0 flex flex-col overflow-hidden">
+      <div className="flex gap-3.5 items-center px-4 py-4">
+        <div
+          className="translate-y-0.5 rounded-full p-1 hover:bg-slate-100"
+          onClick={() => {
+            dispatch(popPage({ stack } as PageLocator));
+          }}
+        >
+          {page.closeable && <FaXmark size={18} />}
+        </div>
+        <div className="text-2xl font-medium">{Component.label}</div>
+      </div>
+      <Component.component />
+    </div>
+  );
 }
 
 export default PageRenderer;
