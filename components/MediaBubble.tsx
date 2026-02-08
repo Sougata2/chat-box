ae632dd03126eaf9c4dc43a1a3f4467c6180d41f
sougata <sougata19582001@gmail.com>
2026-02-08 23:31:14 +0530

    added sender name and creation timestamp with some styple adjustments

--- components/MediaBubble.tsx
+++ components/MediaBubble.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import {
-  Dialog,
+  DialogTrigger,
   DialogContent,
   DialogTitle,
-  DialogTrigger,
+  Dialog,
 } from "@/components/ui/dialog";
 import { MdOutlineFileDownload } from "react-icons/md";
+import { getNameColor } from "@/lib/utils";
 import { useSelector } from "react-redux";
 import { toastError } from "./toastError";
 import { RootState } from "@/app/store/store";
+import { TbChecks } from "react-icons/tb";
+import { FiClock } from "react-icons/fi";
 import { Message } from "@/app/types/room";
+import { format } from "date-fns";
 import { Media } from "@/app/types/media";
 
 import Image from "next/image";
@@ -29,8 +33,6 @@ function MediaBubble({
   const token = useSelector((state: RootState) => state.user.accessToken);
   const previewMedia = media.slice(0, MAX_PREVIEW);
   const remaining = media.length - MAX_PREVIEW;
-
-  console.log(msg);
 
   async function download(url: string) {
     if (!url) return;
@@ -57,144 +59,199 @@ function MediaBubble({
       `}
     >
       <div
-        className="
+        className={`
           grid grid-cols
-          gap-2
-        "
+          p-1
+          max-w-md
+          rounded-lg border
+          gap-1 wrap-anywhere [word-break:break-word]
+          ${isMe ? "bg-emerald-200 text-emerald-800" : "bg-white"}
+        `}
       >
-        {previewMedia.map((m, index) => {
-          const isLast = index === MAX_PREVIEW - 1 && remaining > 0;
+        {!isMe && (
+          <div
+            className={`
+              h-3.5
+              mt-1 px-1
+              text-xs font-semibold
+              -translate-y-1 capitalize
+              ${getNameColor(msg.sender.firstName.toLowerCase())}
+            `}
+          >
+            {msg.sender.firstName} {msg.sender.lastName}
+          </div>
+        )}
+        <div
+          className="
+            flex flex-col gap-1 w-full min-w-52
+          "
+        >
+          {previewMedia.map((m, index) => {
+            const isLast = index === MAX_PREVIEW - 1 && remaining > 0;
 
-          return (
-            <Dialog key={m.id}>
-              <DialogTrigger>
-                <div
+            return (
+              <Dialog key={m.id}>
+                <DialogTrigger asChild>
+                  <div
+                    className={`
+              relative
+              w-full
+              h-44
+              rounded-lg
+              overflow-hidden
+              border
+              cursor-pointer
+              ${isMe ? "border-emerald-400" : "border-slate-300"}
+            `}
+                  >
+                    <Image
+                      src={`${process.env.NEXT_PUBLIC_SERVER_URL}/chat-service${m.url}?token=${token}`}
+                      alt="media"
+                      fill
+                      unoptimized
+                      className="object-cover"
+                    />
+
+                    {isLast && (
+                      <div className="absolute inset-0 bg-black/60 flex items-center justify-center text-white text-xl font-semibold">
+                        +{remaining}
+                      </div>
+                    )}
+                  </div>
+                </DialogTrigger>
+                <DialogContent
+                  showCloseButton={false}
                   className="
-                    overflow-hidden
-                    w-30 h-30
-                    rounded-lg
-                    relative
+                    flex flex-col
+                    max-w-none! w-fit
+                    p-0
+                    bg-black/80
+                    border-none
+                    shadow-none
+                    backdrop-blur-md gap-2 items-center justify-center
                   "
                 >
-                  <Image
-                    src={`${process.env.NEXT_PUBLIC_SERVER_URL}/chat-service${m.url}?token=${token}`}
-                    alt="media"
-                    fill
-                    unoptimized
+                  <DialogTitle
                     className="
-                      object-cover
+                      flex
+                      w-full
+                      px-4 pt-3 pb-0
+                      text-white
+                      items-center justify-between
                     "
-                  />
-
-                  {isLast && (
+                  >
+                    <p
+                      className="
+                        max-w-[70vw]
+                        text-sm font-medium
+                        truncate
+                      "
+                    >
+                      {m.originalName}
+                    </p>
                     <div
                       className="
                         flex
-                        text-white text-xl font-semibold
-                        bg-black/60
-                        absolute inset-0 items-center justify-center
                       "
                     >
-                      +{remaining}
+                      <button
+                        onClick={async () => await download(m.url ?? "")}
+                        className="
+                          flex
+                          w-8 h-8
+                          ml-4
+                          text-white text-lg
+                          bg-white/10
+                          rounded-full
+                          items-center justify-center hover:bg-white/20
+                        "
+                      >
+                        <MdOutlineFileDownload />
+                      </button>
+                      <button
+                        onClick={() =>
+                          document.dispatchEvent(
+                            new KeyboardEvent("keydown", { key: "Escape" }),
+                          )
+                        }
+                        className="
+                          flex
+                          w-8 h-8
+                          ml-4
+                          text-white text-lg
+                          bg-white/10
+                          rounded-full
+                          items-center justify-center hover:bg-white/20
+                        "
+                      >
+                        ✕
+                      </button>
                     </div>
-                  )}
-                </div>
-              </DialogTrigger>
-              <DialogContent
-                showCloseButton={false}
-                className="
-                  flex flex-col
-                  max-w-none! w-fit
-                  p-0
-                  bg-black/80
-                  border-none
-                  shadow-none
-                  backdrop-blur-md gap-2 items-center justify-center
-                "
-              >
-                <DialogTitle
-                  className="
-                    flex
-                    w-full
-                    px-4 pt-3 pb-0
-                    text-white
-                    items-center justify-between
-                  "
-                >
-                  {/* HEADER */}
-                  <p
+                  </DialogTitle>
+                  <div
                     className="
-                      max-w-[70vw]
-                      text-sm font-medium
-                      truncate
-                    "
-                  >
-                    {m.originalName}
-                  </p>
-
-                  {/* close button (custom) */}
-                  <div className="flex">
-                    <button
-                      className="
-                      flex
-                      w-8 h-8
-                      ml-4
-                      text-white text-lg
-                      bg-white/10
-                      rounded-full
-                      items-center justify-center hover:bg-white/20
-                    "
-                      onClick={async () => await download(m.url ?? "")}
-                    >
-                      <MdOutlineFileDownload />
-                    </button>
-                    <button
-                      onClick={() =>
-                        document.dispatchEvent(
-                          new KeyboardEvent("keydown", { key: "Escape" }),
-                        )
-                      }
-                      className="
-                    flex
-                    w-8 h-8
-                    ml-4
-                    text-white text-lg
-                    bg-white/10
-                    rounded-full
-                    items-center justify-center hover:bg-white/20
-                    "
-                    >
-                      ✕
-                    </button>
-                  </div>
-                </DialogTitle>
-
-                {/* IMAGE */}
-                <div
-                  className="
                     flex
                     px-4 pb-6
                     items-center justify-center
-                  "
-                >
-                  <Image
-                    src={`${process.env.NEXT_PUBLIC_SERVER_URL}/chat-service${m.url}?token=${token}`}
-                    alt="preview"
-                    width={1600}
-                    height={1600}
-                    unoptimized
-                    className="
+                    "
+                  >
+                    <Image
+                      src={`${process.env.NEXT_PUBLIC_SERVER_URL}/chat-service${m.url}?token=${token}`}
+                      alt="preview"
+                      width={1600}
+                      height={1600}
+                      unoptimized
+                      className="
                       object-contain
                       w-auto h-auto max-w-[92vw] max-h-[85vh]
                       rounded-lg
                       shadow-2xl
-                    "
-                  />
-                </div>
-              </DialogContent>
-            </Dialog>
-          );
-        })}
+                      "
+                    />
+                  </div>
+                </DialogContent>
+              </Dialog>
+            );
+          })}
+        </div>
+        {msg.message && (
+          <div
+            className="
+            px-1
+          "
+          >
+            {msg.message}
+          </div>
+        )}
+        <div
+          className="
+            flex
+            h-2.5
+            justify-end
+          "
+        >
+          <div
+            className="
+              flex
+              text-[11px] text-slate-600
+              items-center gap-1
+            "
+          >
+            {format(
+              msg?.createdAt ? new Date(msg?.createdAt) : new Date(),
+              "hh:mm aaa",
+            )}
+            {!msg?.createdAt && <FiClock size={11} />}
+            {isMe && msg?.createdAt && (
+              <TbChecks
+                size={20}
+                className="
+                  text-emerald-700
+                "
+              />
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
