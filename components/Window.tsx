3c5e593ff8b58965bc9ba878720f3747eb889c40
sougata <sougata19582001@gmail.com>
2026-02-15 20:15:06 +0530

    added dropdown menu. Added mute/unmute notification functionality

--- components/Window.tsx
+++ components/Window.tsx
@@ -1,16 +1,45 @@
 "use client";
 
+import {
+  DropdownMenuRadioGroup,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSub,
+  DropdownMenu,
+} from "@/components/ui/dropdown-menu";
+import {
+  CalendarPlusIcon,
+  MoreVerticalIcon,
+  ListFilterIcon,
+  MailCheckIcon,
+  ArchiveIcon,
+  Trash2Icon,
+  BellOff,
+  TagIcon,
+  Bell,
+} from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/app/store/store";
 import { Page, PageLocator } from "@/app/types/page";
 import { GroupAvatar } from "./GroupAvatar";
+import { toastError } from "./toastError";
+import { updateRoom } from "@/app/store/chatSlice";
 import { stackPage } from "@/app/store/pageSlice";
 import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { format } from "date-fns";
+import { chat } from "@/app/clients/chatClient";
+import { Room } from "@/app/types/room";
 import { User } from "@/app/types/user";
 
 import PageRenderer from "./PageRenderer";
-import { format } from "date-fns";
 
 function Window() {
   const dispatch = useDispatch<AppDispatch>();
@@ -32,6 +61,37 @@ function Window() {
     );
   }, [dispatch]);
 
+  async function muteRoom() {
+    if (!user?.email) return;
+    if (!room?.mutedParticipants) return;
+    const mutedParticipants = [...room?.mutedParticipants];
+    if (mutedParticipants?.includes(user?.email)) return;
+    mutedParticipants?.push(user.email);
+    const updatedRoom = { ...room, mutedParticipants } as Room;
+    dispatch(updateRoom({ ...updatedRoom } as Room));
+    try {
+      chat.post(`/rooms/mute-room/${room.referenceNumber}`);
+    } catch (error) {
+      toastError(error);
+    }
+  }
+
+  async function unmuteRoom() {
+    if (!user?.email) return;
+    if (!room?.mutedParticipants) return;
+    const mutedParticipants = [...room?.mutedParticipants];
+    const index = mutedParticipants?.indexOf(user.email);
+    console.log(index);
+    mutedParticipants?.splice(index, 1);
+    const updatedRoom = { ...room, mutedParticipants };
+    dispatch(updateRoom({ ...updatedRoom } as Room));
+    try {
+      chat.post(`/rooms/unmute-room/${room.referenceNumber}`);
+    } catch (error) {
+      toastError(error);
+    }
+  }
+
   return (
     <div
       className="
@@ -42,84 +102,165 @@ function Window() {
       "
     >
       <div
-        className="
-          flex
-          h-16
-          px-4
-          bg-white
-          rounded-lg border border-slate-300
-          items-center gap-3
-        "
+        className="flex justify-between px-4 items-center bg-white
+          rounded-lg border border-slate-300"
       >
-        {room?.groupName && <GroupAvatar />}
-        {!room?.groupName && (
-          <Avatar
-            className="
-              h-10 w-10
-            "
-          >
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>
-              {otherParticipant?.firstName?.[0]}
-              {otherParticipant?.lastName?.[0]}
-            </AvatarFallback>
-          </Avatar>
-        )}
-
         <div
           className="
+        flex
+        h-16
+          items-center gap-3
+        "
+        >
+          {room?.groupName && <GroupAvatar />}
+          {!room?.groupName && (
+            <Avatar
+              className="
+              h-10 w-10
+              "
+            >
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>
+                {otherParticipant?.firstName?.[0]}
+                {otherParticipant?.lastName?.[0]}
+              </AvatarFallback>
+            </Avatar>
+          )}
+
+          <div
+            className="
             flex flex-col
           "
-        >
-          {room?.groupName && (
-            <span
-              className="
+          >
+            {room?.groupName && (
+              <span
+                className="
                 text-slate-700 font-medium
                 capitalize
               "
-            >
-              {room?.groupName}
-            </span>
-          )}
-          {!room?.groupName && (
-            <span
-              className="
+              >
+                {room?.groupName}
+              </span>
+            )}
+            {!room?.groupName && (
+              <span
+                className="
                 text-slate-700 font-medium
                 capitalize
               "
-            >
-              {otherParticipant?.firstName} {otherParticipant?.lastName}
-            </span>
-          )}
-          {room?.groupName && (
-            <span
-              className="
+              >
+                {otherParticipant?.firstName} {otherParticipant?.lastName}
+              </span>
+            )}
+            {room?.groupName && (
+              <span
+                className="
                 text-xs text-slate-500
               "
-            >
-              {room?.participants
-                .map((p) => `${p.firstName} ${p.lastName}`)
-                .join(", ")}
-            </span>
-          )}
-          {!room?.groupName && (
-            <span
-              className="
+              >
+                {room?.participants
+                  .map((p) => `${p.firstName} ${p.lastName}`)
+                  .join(", ")}
+              </span>
+            )}
+            {!room?.groupName && (
+              <span
+                className="
                 text-xs text-slate-500 lowercase
               "
-            >
-              {presenceMap[otherParticipant?.email ?? ""].status ===
-                "ONLINE" && <span className="text-emerald-500">● </span>}
-              {presenceMap[otherParticipant?.email ?? ""].status === "OFFLINE"
-                ? `last seen • ${format(
-                    new Date(
-                      presenceMap[otherParticipant?.email ?? ""].lastSeen,
-                    ),
-                    "hh:mm aaa (dd-MM-yy)",
-                  )}`
-                : presenceMap[otherParticipant?.email ?? ""].status}
-            </span>
-          )}
+              >
+                {presenceMap[otherParticipant?.email ?? ""]?.status ===
+                  "ONLINE" && <span className="text-emerald-500">● </span>}
+                {presenceMap[otherParticipant?.email ?? ""]?.status ===
+                "OFFLINE"
+                  ? `last seen • ${format(
+                      new Date(
+                        presenceMap[otherParticipant?.email ?? ""]?.lastSeen,
+                      ),
+                      "hh:mm aaa (dd-MM-yy)",
+                    )}`
+                  : presenceMap[otherParticipant?.email ?? ""]?.status}
+              </span>
+            )}
+          </div>
+        </div>
+        <div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="ghost"
+                size="icon"
+                aria-label="More Options"
+                className="rounded-full focus:ring-0 focus-visible:ring-0 focus:outline-none"
+              >
+                <MoreVerticalIcon />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-40">
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <MailCheckIcon />
+                  Mark as Read
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <ArchiveIcon />
+                  Archive
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                {user?.email && (
+                  <>
+                    {room?.mutedParticipants.includes(user?.email) ? (
+                      <DropdownMenuItem onClick={unmuteRoom}>
+                        <Bell />
+                        Unmute Notification
+                      </DropdownMenuItem>
+                    ) : (
+                      <DropdownMenuItem onClick={muteRoom}>
+                        <BellOff />
+                        Mute Notification
+                      </DropdownMenuItem>
+                    )}
+                  </>
+                )}
+                <DropdownMenuItem>
+                  <CalendarPlusIcon />
+                  Add to Calendar
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <ListFilterIcon />
+                  Add to List
+                </DropdownMenuItem>
+                <DropdownMenuSub>
+                  <DropdownMenuSubTrigger>
+                    <TagIcon />
+                    Label As...
+                  </DropdownMenuSubTrigger>
+                  <DropdownMenuSubContent>
+                    <DropdownMenuRadioGroup value={"label"}>
+                      <DropdownMenuRadioItem value="personal">
+                        Personal
+                      </DropdownMenuRadioItem>
+                      <DropdownMenuRadioItem value="work">
+                        Work
+                      </DropdownMenuRadioItem>
+                      <DropdownMenuRadioItem value="other">
+                        Other
+                      </DropdownMenuRadioItem>
+                    </DropdownMenuRadioGroup>
+                  </DropdownMenuSubContent>
+                </DropdownMenuSub>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem variant="destructive">
+                  <Trash2Icon />
+                  Trash
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
 
