3bec540e283f8549f6483b2760a3729d3b5da72f
sougata <sougatadas@cdac.in>
2026-03-09 15:19:22 +0530

    added whatapp doodle to the background and background image from whatsapp itself.

--- app/(application)/layout.tsx
+++ app/(application)/layout.tsx
@@ -3,15 +3,22 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
 
-import React from "react";
 import WebSocketProvider from "@/provider/WebSocketProvider";
+import React from "react";
 
 function Layout({ children }: { children: React.ReactNode }) {
   const { accessToken } = useSelector((state: RootState) => state.user);
 
   return (
     <WebSocketProvider token={accessToken}>
-      <div className="bg-slate-100 h-screen">{children}</div>
+      <div
+        className="h-screen bg-repeat bg-size-[350px] bg-slate-400 dark:bg-slate-900"
+        style={{ backgroundImage: "url('/whats-doodles.png')" }}
+      >
+        <div className="h-full bg-white/75 dark:bg-slate-900/70">
+          {children}
+        </div>
+      </div>
     </WebSocketProvider>
   );
 }
